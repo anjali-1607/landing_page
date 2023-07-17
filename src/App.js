c00ed14e85395aad304f360222803dff506662c8
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-07-18 00:52:48 +0530

    _done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
 import "./App.css";
 import Home from "./components/Home";
+import About from "./components/About";
+import Work from "./components/Work";
+import Testimonial from "./components/Testimonial";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
-export default function App() {
+function App() {
   return (
-    <div>
-      {" "}
-      <Home />{" "}
+    <div className="App">
+      <Home />
+      <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
 }
+
+export default App;
